fa742a115b8358dd1dbb6101eeb62b2be8d2e8eb
andysem <andysem@ab6a8de6-4e00-4c3d-a860-8899c17d353f>
2007-10-28 14:16:12 +0000

    Added a simplier way to extract attribute value if one knows its exact type
    git-svn-id: https://boost-log.svn.sourceforge.net/svnroot/boost-log/trunk/boost-log@20 ab6a8de6-4e00-4c3d-a860-8899c17d353f

--- boost/log/attributes/attribute.hpp
+++ boost/log/attributes/attribute.hpp
@@ -19,7 +19,12 @@
 #ifndef BOOST_LOG_ATTRIBUTE_HPP_INCLUDED_
 #define BOOST_LOG_ATTRIBUTE_HPP_INCLUDED_
 
+#include <boost/optional.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/type_traits/add_const.hpp>
+#include <boost/type_traits/add_reference.hpp>
+#include <boost/type_traits/remove_cv.hpp>
+#include <boost/type_traits/remove_reference.hpp>
 #include <boost/log/detail/prologue.hpp>
 #include <boost/log/type_dispatch/type_dispatcher.hpp>
 
@@ -35,6 +40,54 @@ struct BOOST_LOG_NO_VTABLE attribute_value
     //! The method dispatches the value to the given object. It returns true if the
     //! object was capable to consume the real attribute value type and false otherwise.
     virtual bool dispatch(type_dispatcher& dispatcher) = 0;
+
+    //! A simplier way to get attribute value in case if one knows its exact type
+    template< typename T >
+    optional<
+        typename add_reference<
+            typename add_const<
+                typename remove_cv<
+                    typename remove_reference< T >::type
+                >::type
+            >::type
+        >::type
+    > get()
+    {
+        typedef typename remove_cv<
+            typename remove_reference< T >::type
+        >::type requested_type;
+        typedef optional<
+            typename add_reference<
+                typename add_const< requested_type >::type
+            >::type
+        > result_type;
+
+        struct local_type_dispatcher :
+            public type_dispatcher,
+            public type_visitor< requested_type >
+        {
+            explicit local_type_dispatcher(result_type& res) : res_(res) {}
+            void* get_visitor(std::type_info const& type)
+            {
+                if (type == typeid(requested_type))
+                    return static_cast< type_visitor< requested_type >* >(this);
+                else
+                    return 0;
+            }
+            void visit(requested_type const& value)
+            {
+                res_ = value;
+            }
+
+        private:
+            result_type& res_;
+        };
+
+        result_type res;
+        local_type_dispatcher disp(res);
+        this->dispatch(disp);
+        return res;
+    }
 };
 
 //! A base class for an attribute
