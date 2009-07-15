d0449814abf6a3eca1ded0e1f3640c39090ccda1
andysem <andysem@ab6a8de6-4e00-4c3d-a860-8899c17d353f>
2009-07-15 16:58:32 +0000

    File sink.cpp renamed to sink_frontends.cpp
    git-svn-id: https://boost-log.svn.sourceforge.net/svnroot/boost-log/trunk/boost-log@318 ab6a8de6-4e00-4c3d-a860-8899c17d353f

--- libs/log/src/sink_frontends.cpp
+++ libs/log/src/sink_frontends.cpp
@@ -4,7 +4,7 @@
  * Use, modification and distribution is subject to the Boost Software License, Version 1.0.
  * (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
  *
- * \file   sink.cpp
+ * \file   sink_frontends.cpp
  * \author Andrey Semashev
  * \date   03.11.2007
  *
