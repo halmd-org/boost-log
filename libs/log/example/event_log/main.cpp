d212ee53fe46860aad24f122e64ea9c553aa0e29
andysem <andysem@ab6a8de6-4e00-4c3d-a860-8899c17d353f>
2010-05-17 16:57:29 +0000

    Extracted more code snippets into standalone examples.
    git-svn-id: https://boost-log.svn.sourceforge.net/svnroot/boost-log/trunk/boost-log@470 ab6a8de6-4e00-4c3d-a860-8899c17d353f

--- libs/log/example/event_log/main.cpp
+++ libs/log/example/event_log/main.cpp
@@ -11,14 +11,12 @@
  *
  * \brief  An example of logging into Windows event log.
  *
- * The example shows the basic usage of the simple Windows NT event log backend.
+ * The example shows the basic usage of the Windows NT event log backend.
  * The code defines custom severity levels, initializes the sink and a couple of
  * attributes to test with, and writes several records at different levels.
  * As a result the written records should appear in the Application log, and
  * should be displayed correctly with the Windows event log viewer.
  */
-
-#define BOOST_LOG_DYN_LINK 1
 
 #include <stdexcept>
 #include <string>
@@ -32,6 +30,8 @@
 #include <boost/log/sinks/sync_frontend.hpp>
 #include <boost/log/sinks/event_log_backend.hpp>
 
+#include "event_log_messages.h"
+
 namespace logging = boost::log;
 namespace attrs = boost::log::attributes;
 namespace src = boost::log::sources;
@@ -39,53 +39,142 @@ namespace sinks = boost::log::sinks;
 namespace fmt = boost::log::formatters;
 namespace keywords = boost::log::keywords;
 
-using boost::shared_ptr;
-
-//! Define application-specific severity levels
-enum severity_levels
+//[ example_sinks_event_log_severity
+// Define application-specific severity levels
+enum severity_level
 {
     normal,
     warning,
     error
 };
+//]
+
+void init_logging()
+{
+    //[ example_sinks_event_log_create_backend
+    // Create an event log sink
+    boost::shared_ptr< sinks::event_log_backend > backend(
+        new sinks::event_log_backend((
+            keywords::message_file = "%SystemDir%\\event_log_messages.dll",
+            keywords::log_name = "My Application",
+            keywords::log_source = "My Source"
+        ))
+    );
+    //]
+
+    //[ example_sinks_event_log_event_composer
+    // Create an event composer. It is initialized with the event identifier mapping.
+    sinks::event_log::event_composer composer(
+        sinks::event_log::direct_event_id_mapping< int >("EventID"));
+
+    // For each event described in the message file, set up the insertion string formatters
+    composer[LOW_DISK_SPACE_MSG]
+        // the first placeholder in the message
+        // will be replaced with contents of the "Drive" attribute
+        % fmt::attr< std::string >("Drive")
+        // the second placeholder in the message
+        // will be replaced with contents of the "Size" attribute
+        % fmt::attr< boost::uintmax_t >("Size");
+
+    composer[DEVICE_INACCESSIBLE_MSG]
+        % fmt::attr< std::string >("Drive");
+
+    composer[SUCCEEDED_MSG]
+        % fmt::attr< unsigned int >("Duration");
+
+    // Then put the composer to the backend
+    backend->set_event_composer(composer);
+    //]
+
+    //[ example_sinks_event_log_mappings
+    // We'll have to map our custom levels to the event log event types
+    sinks::event_log::custom_event_type_mapping< severity_level > type_mapping("Severity");
+    type_mapping[normal] = sinks::event_log::make_event_type(MY_SEVERITY_INFO);
+    type_mapping[warning] = sinks::event_log::make_event_type(MY_SEVERITY_WARNING);
+    type_mapping[error] = sinks::event_log::make_event_type(MY_SEVERITY_ERROR);
+
+    backend->set_event_type_mapper(type_mapping);
+
+    // Same for event categories.
+    // Usually event categories can be restored by the event identifier.
+    sinks::event_log::custom_event_category_mapping< int > cat_mapping("EventID");
+    cat_mapping[LOW_DISK_SPACE_MSG] = sinks::event_log::make_event_category(MY_CATEGORY_1);
+    cat_mapping[DEVICE_INACCESSIBLE_MSG] = sinks::event_log::make_event_category(MY_CATEGORY_2);
+    cat_mapping[SUCCEEDED_MSG] = sinks::event_log::make_event_category(MY_CATEGORY_3);
+
+    backend->set_event_category_mapper(cat_mapping);
+    //]
+
+    //[ example_sinks_event_log_register_sink
+    // Create the frontend for the sink
+    boost::shared_ptr< sinks::synchronous_sink< sinks::event_log_backend > > sink(
+        new sinks::synchronous_sink< sinks::event_log_backend >(backend));
+
+    // Set up filter to pass only records that have the necessary attribute
+    sink->set_filter(flt::has_attr< int >("EventID"));
+
+    logging::core::get()->add_sink(sink);
+    //]
+}
+
+//[ example_sinks_event_log_facilities
+BOOST_LOG_DECLARE_GLOBAL_LOGGER(event_logger, src::severity_logger_mt< severity_level >)
+
+// The function raises an event of the disk space depletion
+void announce_low_disk_space(std::string const& drive, boost::uintmax_t size)
+{
+    BOOST_LOG_SCOPED_THREAD_TAG("EventID", int, LOW_DISK_SPACE_MSG);
+    BOOST_LOG_SCOPED_THREAD_TAG("Drive", std::string, drive);
+    BOOST_LOG_SCOPED_THREAD_TAG("Size", boost::uintmax_t, size);
+    // Since this record may get accepted by other sinks,
+    // this message is not completely useless
+    BOOST_LOG_SEV(event_logger::get(), warning) << "Low disk " << drive
+        << " space, " << size << " Mb is recommended";
+}
+
+// The function raises an event of inaccessible disk drive
+void announce_device_inaccessible(std::string const& drive)
+{
+    BOOST_LOG_SCOPED_THREAD_TAG("EventID", int, DEVICE_INACCESSIBLE_MSG);
+    BOOST_LOG_SCOPED_THREAD_TAG("Drive", std::string, drive);
+    BOOST_LOG_SEV(event_logger::get(), error) << "Cannot access drive " << drive;
+}
+
+// The structure is an activity guard that will emit an event upon the activity completion
+struct activity_guard
+{
+    activity_guard()
+    {
+        // Add a stop watch attribute to measure the activity duration
+        boost::shared_ptr< attrs::timer > attr(new attrs::timer());
+        m_it = event_logger::get().add_attribute("Duration", attr);
+    }
+    ~activity_guard()
+    {
+        BOOST_LOG_SCOPED_THREAD_TAG("EventID", int, SUCCEEDED_MSG);
+        BOOST_LOG_SEV(event_logger::get(), normal) << "Activity ended";
+        event_logger::get().remove_attribute(m_it);
+    }
+
+private:
+    event_logger::logger_type::attribute_set_type::iterator m_it;
+};
+//]
 
 int main(int argc, char* argv[])
 {
     try
     {
-        // Create an event log sink
-        shared_ptr< sinks::synchronous_sink< sinks::simple_event_log_backend > > sink(
-            new sinks::synchronous_sink< sinks::simple_event_log_backend >());
+        // Initialize the library
+        init_logging();
 
-        sink->locked_backend()->set_formatter(
-            fmt::format("%1%: [%2%] - %3%")
-                % fmt::attr< unsigned int >("Line #")
-                % fmt::date_time< boost::posix_time::ptime >("TimeStamp")
-                % fmt::message()
-            );
+        // Make some events
+        {
+            activity_guard activity;
 
-        // We'll have to map our custom levels to the event log event types
-        sinks::event_log::custom_event_type_mapping< severity_levels > mapping("Severity");
-        mapping[normal] = sinks::event_log::info;
-        mapping[warning] = sinks::event_log::warning;
-        mapping[error] = sinks::event_log::error;
-
-        sink->locked_backend()->set_event_type_mapper(mapping);
-
-        // Add the sink to the core
-        logging::core::get()->add_sink(sink);
-
-        // Add some attributes too
-        shared_ptr< logging::attribute > attr(new attrs::local_clock);
-        logging::core::get()->add_global_attribute("TimeStamp", attr);
-        attr.reset(new attrs::counter< unsigned int >);
-        logging::core::get()->add_global_attribute("Line #", attr);
-
-        // Do some logging
-        src::severity_logger< severity_levels > lg(keywords::severity = normal);
-        BOOST_LOG_SEV(lg, normal) << "Some record for NT event log with normal level";
-        BOOST_LOG_SEV(lg, warning) << "Some record for NT event log with warning level";
-        BOOST_LOG_SEV(lg, error) << "Some record for NT event log with error level";
+            announce_low_disk_space("C:", 2 * 1024 * 1024);
+            announce_device_inaccessible("D:");
+        }
 
         return 0;
     }
