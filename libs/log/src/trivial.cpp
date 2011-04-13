44f5cb9642d14c47c875a2da18937acb3786f0e5
andysem <andysem@ab6a8de6-4e00-4c3d-a860-8899c17d353f>
2011-04-13 18:58:58 +0000

    Replaced BOOST_CYGWIN with __CYGWIN__ as the former seems to be removed in Boost trunk.
    git-svn-id: https://boost-log.svn.sourceforge.net/svnroot/boost-log/trunk/boost-log@595 ab6a8de6-4e00-4c3d-a860-8899c17d353f

--- libs/log/src/trivial.cpp
+++ libs/log/src/trivial.cpp
@@ -93,7 +93,7 @@ BOOST_LOG_ANONYMOUS_NAMESPACE {
 
     log::aux::universal_path suggest_file_name()
     {
-#if defined(BOOST_HAS_UNISTD_H) && !defined(BOOST_CYGWIN)
+#if defined(BOOST_HAS_UNISTD_H) && !defined(__CYGWIN__)
 
         // Attempt to read the executable name from the proc filesystem
         char buf[FILENAME_MAX];
@@ -110,7 +110,7 @@ BOOST_LOG_ANONYMOUS_NAMESPACE {
                 filesystem::basename(log::aux::to_universal_path(full_name)) + ext);
         }
 
-#endif // defined(BOOST_HAS_UNISTD_H) && !defined(BOOST_CYGWIN)
+#endif // defined(BOOST_HAS_UNISTD_H) && !defined(__CYGWIN__)
 
         return log::aux::to_universal_path("application.log");
     }
