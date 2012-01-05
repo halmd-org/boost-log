f1860a2577f6a123f79c4eb09a3ed5cbc4072d8d
andysem <andysem@ab6a8de6-4e00-4c3d-a860-8899c17d353f>
2012-01-05 10:17:21 +0000

    Added license comment.
    git-svn-id: https://boost-log.svn.sourceforge.net/svnroot/boost-log/branches/bleeding-edge@639 ab6a8de6-4e00-4c3d-a860-8899c17d353f

--- boost/log/utility/type_info_wrapper.hpp
+++ boost/log/utility/type_info_wrapper.hpp
@@ -1,3 +1,9 @@
+/*
+ *          Copyright Andrey Semashev 2007 - 2012.
+ * Distributed under the Boost Software License, Version 1.0.
+ *    (See accompanying file LICENSE_1_0.txt or copy at
+ *          http://www.boost.org/LICENSE_1_0.txt)
+ */
 /*!
  * \file   type_info_wrapper.hpp
  * \author Andrey Semashev
