592fb4f3b5db98b53211972666d26183d7368853
andysem <andysem@ab6a8de6-4e00-4c3d-a860-8899c17d353f>
2012-12-01 20:40:07 +0000

    Added add_attr to the test.
    git-svn-id: https://boost-log.svn.sourceforge.net/svnroot/boost-log/branches/bleeding-edge@764 ab6a8de6-4e00-4c3d-a860-8899c17d353f

--- libs/log/example/advanced_usage/main.cpp
+++ libs/log/example/advanced_usage/main.cpp
@@ -29,6 +29,7 @@
 #include <boost/log/sinks.hpp>
 #include <boost/log/sources/logger.hpp>
 #include <boost/log/utility/empty_deleter.hpp>
+#include <boost/log/utility/manipulators/add_attr.hpp>
 #include <boost/log/attributes/scoped_attribute.hpp>
 #include <boost/log/support/date_time.hpp>
 
@@ -241,6 +242,7 @@ int main(int argc, char* argv[])
 
     // And this line is not highlighted anymore
     BOOST_LOG(lg) << "Now the tag is removed";
+    BOOST_LOG(lg) << logging::add_attr("Tag", std::string("Tagged line")) << "Some lines can also be selectively tagged";
 
     // Now let's try to apply filtering to the output. Filtering is based on
     // attributes being output with the record. One of the common filtering use cases
